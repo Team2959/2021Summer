22a04d5e1baf0d946698de72da9ba35bc45335a7
samueldaley <stallionsamueld@gmail.com>
2020-06-09 17:32:04 -0400

    Added Encoders

--- src/main/include/subsystems/Drivetrain.h
+++ src/main/include/subsystems/Drivetrain.h
@@ -11,6 +11,13 @@ private:
 	rev::CANSparkMax m_motor2Spin {3, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
 	rev::CANSparkMax m_motor3Wheel{4, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
 	rev::CANSparkMax m_motor3Spin {5, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+
+	rev::CANSparkMax m_motor1WheelEncoder{m_motor1Wheel};
+	rev::CANSparkMax m_motor1SpinEncoder {m_motor1Spin};
+	rev::CANSparkMax m_motor2WheelEncoder{m_motor2Wheel};
+	rev::CANSparkMax m_motor2SpinEncoder {m_motor2Spin};
+	rev::CANSparkMax m_motor3WheelEncoder{m_motor3Wheel};
+	rev::CANSparkMax m_motor3SpinEncoder {m_motor3Spin};
 public:
 };
 
